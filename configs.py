581270e04b4bffaa3dfc7c63743e7f13aa57bd25
henry440 <440henry@gmail.com>
2020-11-28 23:47:48 +0100

    Add Codes

--- configs.py
+++ configs.py
@@ -12,3 +12,14 @@ AUTHOR = "Henry440"
 
 #Protokoll
 WHITELIST_CMD = ("ping", "register")
+
+##CODES
+CODES = {
+    200 : "OK",
+    403 : "Forbidden",
+    500 : "Internal Server Error",
+    900 : "Gegenstelle nicht Erreichbar",
+    901 : "Server nicht Erreichbar",
+    910 : "Argument Error",
+    911 : "Permission Error"
+}

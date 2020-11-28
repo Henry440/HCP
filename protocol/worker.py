2f2230d198060636f8c341e70f8900611fdff9c8
henry440 <440henry@gmail.com>
2020-11-28 19:17:01 +0100

    Init first concept of the Protocol

---
+++ protocol/worker.py
@@ -0,0 +1,16 @@
+#Libarys
+import pickle
+
+class network():
+
+    def __init__(self):
+        self.init = False #Send or recice have to used to unlock th object
+        self.type = 0     #Type 0 is Lock, 1 is send, 2 is recive
+
+    def send(self, data):
+        self.init = True
+        self.type = 1
+
+    def recive(self, data):
+        self.init = True
+        self.type = 2

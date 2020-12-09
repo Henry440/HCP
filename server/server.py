1826e692e01efe02f4b2a5e0aa72197025d2f603
henry440 <440henry@gmail.com>
2020-12-09 16:54:05 +0100

    Server accept now Client Connections

--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 #Libarys
 import socket
+from threading import Thread
 #From Project
 from configs import S_KEY_FILE, SERVER_PORT, SERVER_IP
 from protocol.command import cmd as cmds
@@ -14,4 +15,19 @@ class server():
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.ip, SERVER_PORT))
+        self.socket.listen()
+        self.thread = Thread(target=self._socket_handler)
+        self.thread.start()
+
+    def _socket_handler(self):
+        try:
+            while True:
+                (sock, addr) = self.socket.accept()
+                print(f"New Connection from {addr}")
+        except Exception as e:
+            print("Fehler in Server _socket_handler")
+            print(e)
+
+    def _client_handler(self):
+        pass
 
